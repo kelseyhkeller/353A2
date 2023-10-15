af46384ca2cb51a85a6b06bf2e676a57fe47c9b2
kelseyhkeller <144942806+kelseyhkeller@users.noreply.github.com>
2023-10-15 14:34:04 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = process.env.PORT || 3002;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Welcome to CMPT 353 Tutorials');
+});
+
+app.use('/web'. express.static('pages'));  // lab 2 add in 
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
